2f0f0491c1b21fd58dd96bf39275dab7d4f147af
Ferran Pérez Gamonal <fperez.gamonal@gmail.com>
2019-07-18 11:38:49 +0200

    try converting to tensor + numpy originally as float32

--- src/net.py
+++ src/net.py
@@ -699,11 +699,11 @@ class Net(object):
                 print("After adapt_x, type(sparse_flow_0) : {}".format(type(sparse_flow_0)))
                 print("After adapt_x, frame_0.dtype: {}".format(frame_0.dtype))
                 # Convert numpy arrays to tensors
-                # frame_0, frame_1, matches_0, sparse_flow_0 = self.numpy2tensor(frame_0, frame_1, matches_0,
-                #                                                                sparse_flow_0, input_type=input_type)
+                frame_0, frame_1, matches_0, sparse_flow_0 = self.numpy2tensor(frame_0, frame_1, matches_0,
+                                                                               sparse_flow_0, input_type=input_type)
                 # print("After numpy2tensor, type(frame_0) : {}".format(type(frame_0)))
 
-                if sparse_flow_0 is not None and matches_0 is not None and input_type == 'image_matches':
+                if sparse_flow_0.eval() is not None and matches_0.eval() is not None and input_type == 'image_matches':
                     # frame_0s, frame_1s, matches_0s, sparse_flow_0s = sess.run(frame_0, frame_1, matches_0,
                     #                                                           sparse_flow_0)
                     # print("After sess.run(), type(frame_0s) : {}".format(type(frame_0s)))
