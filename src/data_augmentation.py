2e4cc91b70d93e99446f3dac8170e9ed0bc9531a
Ferran Pérez Gamonal <fperez.gamonal@gmail.com>
2019-08-27 12:58:26 +0200

    wip: pass tuple and then 'unravel' it inside functions(?)(IV)

--- src/data_augmentation.py
+++ src/data_augmentation.py
@@ -356,22 +356,20 @@ def set_range_to_zero(matches, width, offset_h, offset_w, crop_h, crop_w):
 
 
 def corrupt_sparse_flow_loop(matches, density, height=384, width=512):
-    def body(ins):  # what to do once the while loop condition is met
-        matches = ins[0]
-        density = ins[1]
-        height = ins[2]
-        width = ins[3]
-        matches = corrupt_sparse_flow_once(matches, density, height, width)
-        return matches
+    def body(m, d, h, w):  # what to do once the while loop condition is met
+        mtches = corrupt_sparse_flow_once(m, d, h, w)
+        return mtches
 
-    def condition(ins):
+    def condition(m, d, h, w):
         return tf.greater(tf.random_uniform([], maxval=2, dtype=tf.int32), tf.constant(0))
 
     # Perturbate always once (at least)
     matches = corrupt_sparse_flow_once(matches, density, height, width)
     # Draw a random number within 0, 1. If 1, keep corrupting the sparse flow (matches mask) with holes
     inputs = [matches, density, height, width]
-    matches = tf.while_loop(condition, body, inputs)
+    c = lambda m, d, h, w: condition(m, d, h, w)
+    b = lambda m, d, h, w: body(m, d, h, w)
+    matches = tf.while_loop(c, b, loop_vars=[matches, density, height, width])
 
     return matches
 
